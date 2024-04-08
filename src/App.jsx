c925396b90ac6ef92d0fa9296614be5ca1447796
Narotam-Mishra <narotam21jan@gmail.com>
2024-04-08 22:50:29 +0530

    react-real-estate_app_1.10_router_added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,40 @@
 import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
 import Home from "./pages/home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import ListPage from "./pages/listPage/ListPage";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
         <Home />
-      </div>
-    </div>
+      ),
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+  ]);
+  
+  return (
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <Home />
+    //   </div>
+    // </div>
+    
+    <RouterProvider router={router}/>
   )
 }
 
