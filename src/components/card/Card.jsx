68c4dce6a619075f2ba68ce6c6744a873ca3ef4c
Narotam-Mishra <narotam21jan@gmail.com>
2024-04-21 18:47:57 +0530

    react-real-estate_app_1.13_filter_and_listPage_design_1.14

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,44 @@
+/* eslint-disable react/prop-types */
 import './Card.scss';
+import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({ item }) => {
   return (
-    <div className='card'>Card</div>
+    <div className='card'>
+      <Link to={`/${item.id}`} className='imageContainer'>
+        <img src={item.img} alt="card_image" />
+      </Link>
+      <div className="textContainer">
+        <h2 className='title'>
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className='address'>
+          <img src="./pin.png" alt="pinIcon" />
+          <span>{item.address}</span>
+        </p>
+        <p className='price'>$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="./bed.png" alt="bed_image" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="./bath.png" alt="bathtub_image" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="./save.png" alt="saveIcon" />
+            </div>
+            <div className="icon">
+              <img src="./chat.png" alt="chatIcon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
