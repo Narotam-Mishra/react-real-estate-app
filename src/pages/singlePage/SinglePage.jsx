76b9359dfb88df0635b3125378e4ab6c157d77c9
Narotam-Mishra <narotam21jan@gmail.com>
2024-04-28 23:01:59 +0530

    react-real-estate_app_1.17_Property Features Design

--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,6 +1,7 @@
 import Slider from '../../components/slider/Slider';
 import './SinglePage.scss';
 import { singlePostData, userData } from '../../lib/dummyData';
+import Map from '../../components/map/Map';
 
 const SinglePage = () => {
   return (
@@ -30,7 +31,85 @@ const SinglePage = () => {
         </div>
       </div>
       <div className="features">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="./utility.png" alt="utilIcon" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="./pet.png" alt="petIcon" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="./fee.png" alt="feeIcon" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Room Sizes</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="./size.png" alt="sizeIcon" />
+              <span>80 sqft</span>
+            </div>
+            <div className="size">
+              <img src="./bed.png" alt="bedIcon" />
+              <span>2 beds</span>
+            </div>
+            <div className="size">
+              <img src="./bath.png" alt="bathIcon" />
+              <span>1 bathroom</span>
+            </div>
+          </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+          <div className="feature">
+              <img src="/school.png" alt="schoolIcon" />
+              <div className="featureText">
+                <span>School</span>
+                <p>250m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="petIcon" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="feeImage" />
+              <div className="featureText">
+                <span>Restaurant</span>
+                <p>200m away</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]}/>
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="./chat.png" alt="chatIcon" />
+              Send a Message
+            </button>
+            <button>
+              <img src="./save.png" alt="saveIcon" />
+              Save the Place
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
