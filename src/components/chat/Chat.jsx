9d310708be487a401325007b0462c40937a379e9
Narotam-Mishra <narotam21jan@gmail.com>
2024-05-01 16:12:09 +0530

    react-real-estate_app_1.19_chat_component-design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,8 +2,61 @@ import './Chat.scss';
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
+    <div className="chat">
+      <div className="messages">
+        <h1>Messages</h1>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="userImage"
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+      </div>
+      <div className="chatBox">chatBox</div>
+    </div>
+  );
 }
 
 export default Chat
--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import Chat from '../../components/chat/Chat';
 import List from '../../components/list/List';
 import './ProfilePage.scss';
 
@@ -33,7 +34,9 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="chatContainer">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <Chat />
+        </div>
       </div>
     </div>
   );
