6fd41941fe8ab7c5086ae0aeef9987d93bb832f6
Narotam-Mishra <narotam21jan@gmail.com>
2024-08-08 22:55:06 +0530

    react-real-estate_app_1.34_Chat_APIs_added

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,120 +1,91 @@
-import { useState } from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useContext, useState } from 'react';
 import './Chat.scss';
+import { AuthContextVal } from '../../context/AuthContext';
 
-const Chat = () => {
-  const[chat, setChat] = useState(true);
+const Chat = ({ chats }) => {
+  const[chat, setChat] = useState(false);
+  const { currentUser } = useContext(AuthContextVal);
 
+  // console.log(chats)
   return (
     <div className="chat">
       <div className="messages">
         <h1>Messages</h1>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt="userImage"
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-      </div>
-      {chat && <div className="chatBox">
-        <div className="top">
-          <div className="user">
+        {chats?.map((c) => (
+          <div className="message" key={c.id} style={{ backgroundColor: "" }}>
             <img
-              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt=""
+              src={c.receiver.avatar || "/noavatar.jpg"}
+              alt="avatar_image"
             />
-            John Doe
+            <span>{c.receiver.username}</span>
+            <p>{c.lastMessage}</p>
           </div>
-          <span className="close" onClick={() => setChat(null)}>X</span>
+        ))}
+      </div>
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              John Doe
+            </div>
+            <span className="close" onClick={() => setChat(null)}>
+              X
+            </span>
+          </div>
+          <div className="center">
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
         </div>
-        <div className="center">
-          <div className="chatMessage">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage own">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage own">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage own">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage own">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chatMessage own">
-            <p>Lorem ipsum dolor sit amet</p>
-            <span>1 hour ago</span>
-          </div>
-        </div>
-        <div className="bottom">
-          <textarea></textarea>
-          <button>Send</button>
-        </div>
-      </div>}
+      )}
     </div>
   );
 }
